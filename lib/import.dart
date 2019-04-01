9c24b78cdc8a81a2e152246b7faf041dbabc8ce7
Alex <alexandre-del-bigio@alain-bensoussan.com>
2019-04-01 14:42:55 +0200

    can't import dart:html in non browser app

---
+++ lib/import.dart
@@ -0,0 +1,10 @@
+//
+// Issue: https://github.com/dart-lang/pub-dartlang-dart/issues/858
+//
+import 'dart:html' as dart_html;
+import 'dart:js' as dart_js;
+
+/// [dart_html]
+/// [dart_js]
+void fakeInit() {
+}
